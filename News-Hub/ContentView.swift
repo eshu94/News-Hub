4af1813750c0336608daca0ff2fffd62486751a7
Eshita <eshita.narayan94@gmail.com>
2021-02-22 20:28:15 +0530

    Initial Commit

---
+++ News-Hub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  News-Hub
+//
+//  Created by ESHITA on 22/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ News-Hub/News_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_HubApp.swift
+//  News-Hub
+//
+//  Created by ESHITA on 22/02/21.
+//
+
+import SwiftUI
+
+@main
+struct News_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
